13cfa986dd5d8538e8047f64af257ad39e05c239
GermanBluefox <dogafox@gmail.com>
2018-06-18 20:21:11 +0200

    (bluefox) fix tests

--- test/testServer.js
+++ test/testServer.js
@@ -54,15 +54,17 @@ function startClients(_done) {
     const MqttClient = require(__dirname + '/lib/mqttClient.js');
 
     // Start client to emit topics
-    mqttClientEmitter = new MqttClient(() => {
+    mqttClientEmitter = new MqttClient(connected => {
         // on connected
-        console.log('Test MQTT Emitter is connected to MQTT broker');
-        clientConnected1 = true;
-        if (_done && brokerStarted && clientConnected1 && clientConnected2) {
-            _done();
-            _done = null;
+        if (connected) {
+            console.log('Test MQTT Emitter is connected to MQTT broker');
+            clientConnected1 = true;
+            if (_done && brokerStarted && clientConnected1 && clientConnected2) {
+                _done();
+                _done = null;
+            }
         }
-    }, function (topic, message) {
+    }, (topic, message) => {
         console.log(Date.now() + ' emitter received ' + topic + ': ' + message.toString());
         // on receive
         lastReceivedTopic1   = topic;
@@ -70,15 +72,17 @@ function startClients(_done) {
     }, {name: 'Emitter*1', user: 'user', pass: 'pass1'});
 
     // Start client to receive topics
-    mqttClientDetector = new MqttClient(() => {
+    mqttClientDetector = new MqttClient(connected => {
         // on connected
-        console.log('Test MQTT Detector is connected to MQTT broker');
-        clientConnected2 = true;
-        if (_done && brokerStarted && clientConnected1 && clientConnected2) {
-            _done();
-            _done = null;
+        if (connected) {
+            console.log('Test MQTT Detector is connected to MQTT broker');
+            clientConnected2 = true;
+            if (_done && brokerStarted && clientConnected1 && clientConnected2) {
+                _done();
+                _done = null;
+            }
         }
-    }, function (topic, message) {
+    }, (topic, message) => {
         console.log(Date.now() + ' detector received ' + topic + ': ' + message.toString());
         // on receive
         lastReceivedTopic2   = topic;
